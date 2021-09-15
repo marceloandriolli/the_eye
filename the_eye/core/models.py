2db496c4ebe43d96b8ab796ef5b9c8e5e8e23669
Marcelo Andriolli <marcelorsa@gmail.com>
2021-09-15 14:46:33 -0300

    add event models

--- the_eye/core/models.py
+++ the_eye/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class EventType(models.Model):
+    category = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = [['category', 'name']]
+
+
+class Event(models.Model):
+    type = models.ForeignKey(EventType, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    data = models.JSONField()
+
+
+class Session(models.Model):
+    session_id = models.CharField(max_length=200)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
